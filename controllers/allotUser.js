874cb81a98aba1a6a3a67d56c80864b9c197893b
bugger279 <bugger279@gmail.com>
2019-12-25 19:34:33 +0530

    Done With Today

--- controllers/allotUser.js
+++ controllers/allotUser.js
@@ -1,20 +1,53 @@
 const FeedbackModel = require('../models/feedback');
-const response = require('../libs/response');
-const check = require('../libs/checkLib');
-const jwt = require('jsonwebtoken');
 const UserModel = require('../models/user');
 
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
 let allotUser = (req, res) => {
-    UserModel.find({}, function(err, users) {
-        var userMap = {};
-        users.forEach(function(user) {
-          userMap[user._id] = user._id;
+    UserModel.find({}, function (err, users) {
+        var usersArray = users;
+        var selectedUser = usersArray[24];
+        var userTotalCount = usersArray.length;
+        console.log(userTotalCount);
+        
+        var assignedUsers = [];
+        var selectedUserPosition = 24;
+
+
+        while (assignedUsers.length != 3) {
+            var random = randomNumber(0, userTotalCount - 1);
+            if ((selectedUserPosition != random)) { //random no should not be equal to selected user's position
+                if (assignedUsers.indexOf(random) < 0) {
+                    assignedUsers.push(random);                    
+                }
+            }
+        }
+
+        console.log(assignedUsers);
+        assignedUsers.forEach(index => {
+            console.log(usersArray[index]._id);
+        });
+
+
+        // {
+        //     sender_id: assignedUsers,
+        //     receiver_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        //     feedback: { type: String, required: true },
+        //     active: { type: Boolean, default: true },
+        //     received_on: { type: Date, default: Date.now }
+        // }
+
+
+        // 
+        // usersArray.forEach(user => {
+        //     if (user._id !== selectedUser._id) {
+        //         assignedUsers.push(user);
+        //     }
+        // });
+
     });
-    var count = Object.keys(userMap).length;
-    console.log(count);
-    console.log(userMap);
-    res.send({"data" : userMap});
-  });
 }
 
 module.exports = { allotUser };
--- models/giveFeedback.js
+++ models/giveFeedback.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var schema = new Schema({
-    sender_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    content: [{
-        receiver_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-        feedback: { type: String, required: true },
-        received_on: { type: Date, default: Date.now }
-    }]
+    sender_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    receiver_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    feedback: { type: String, required: true },
+    active: { type: Boolean, default: true },
+    received_on: { type: Date, default: Date.now }
 })
 
 module.exports = mongoose.model('Feedback', schema);
